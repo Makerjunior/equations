9a57e93a148ad6fbe6ef89c5234478e8fb8a8159
Mekerjunior <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>
2024-01-02 22:02:19 -0300

    add times feature

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare function solveCubicEquation(a: number, b: number, c: number, d: number): number[];
+export declare function solveQuadraticEquation(a: number, b: number, c: number): number[];
